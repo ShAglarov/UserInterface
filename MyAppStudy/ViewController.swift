1cbc6c0b149d7434d19ce7ecdddbd9b49fe9366e
Aglarov <shamil.aglarov@gmail.com>
2022-07-01 02:24:56 +0300

    Initial Commit

---
+++ MyAppStudy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyAppStudy
+//
+//  Created by Shamil Aglarov on 01.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
